e8a96f59bad01ce6bca97c666ac64a03c031f451
Sergey <seri05@yandex.ru>
2020-03-24 01:09:41 +0300

    implemented request info

---
+++ src/LogicBlock/Info/Implementation/RequestInfo.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace LogicBlock.Info
+{
+    public abstract class RequestInfo : IRequestInfo
+    {
+        public Action<string> OnFail;
+        public Action<string> OnSuccess;
+        
+        public IOperationRequest OperationRequest { get; set; }
+        public byte Status { get; protected set; }
+        public bool IsNew => Status == CallStatus.New;
+        public bool IsSucceeded => Status == CallStatus.Succeeded;
+        public bool IsFailed => Status == CallStatus.Failed;
+        public bool IsProcessed => Status >= CallStatus.Succeeded;
+
+        public void Success(string message)
+        {
+            if (IsProcessed)
+                throw new Exception("Already called Success/Fail");
+            
+            OnSuccess?.Invoke(message);
+
+            Status = CallStatus.Succeeded;
+        }
+
+        public void Fail(string message)
+        {
+            if (IsProcessed)
+                throw new Exception("Already called Success/Fail");
+            
+            Status = CallStatus.Failed;
+
+            OnFail?.Invoke(message);
+        }
+    }
+}
