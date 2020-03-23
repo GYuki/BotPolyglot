f4b0f8184e15114621a013ba15821404a1b1bcdd
Sergey <seri05@yandex.ru>
2020-03-24 00:46:32 +0300

    added request status

---
+++ src/LogicBlock/Info/RequestStatus.cs
@@ -0,0 +1,11 @@
+namespace LogicBlock.Info
+{
+    public class CallStatus
+    {
+        public const byte New = 0;
+
+        public const byte Succeeded = 1;
+
+        public const byte Failed = 2;
+    }
+}
