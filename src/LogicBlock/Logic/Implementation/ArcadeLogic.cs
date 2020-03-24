ef350ded8a2a9a7f8ab10670b369a6cc8d77048d
Sergey <seri05@yandex.ru>
2020-03-25 01:11:05 +0300

    base implementation of logics

---
+++ src/LogicBlock/Logic/Implementation/ArcadeLogic.cs
@@ -0,0 +1,12 @@
+using LogicBlock.Info;
+
+namespace LogicBlock.Logic
+{
+    public class ArcadeLogic : IArcadeLogic
+    {
+        public void HandleText(ITextRequestInfo info)
+        {
+            
+        }
+    }
+}
---
+++ src/LogicBlock/Logic/Implementation/TutorialLogic.cs
@@ -0,0 +1,12 @@
+using LogicBlock.Info;
+
+namespace LogicBlock.Logic
+{
+    public class TutorialLogic : ITutorialLogic
+    {
+        public void HandleText(ITextRequestInfo info)
+        {
+            
+        }
+    }
+}
