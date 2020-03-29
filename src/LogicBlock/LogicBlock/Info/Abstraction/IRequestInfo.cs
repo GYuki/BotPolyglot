677659bae85637cc7a1b66ff1195e07520d9a1d1
Sergey <seri05@yandex.ru>
2020-03-29 20:21:24 +0300

    removed fail and success method

--- src/LogicBlock/LogicBlock/Info/Abstraction/IRequestInfo.cs
+++ src/LogicBlock/LogicBlock/Info/Abstraction/IRequestInfo.cs
@@ -3,20 +3,6 @@ namespace LogicBlock.Info
     public interface IRequestInfo
     {
         IOperationRequest OperationRequest { get; }
-    
-        void Success(IResponseInfo msg);
-
-        void Fail(IResponseInfo msg);
-
-        byte Status { get; }
-
-        bool IsNew { get; }
-
-        bool IsSucceeded { get; }
-
-        bool IsFailed { get; }
-
-        bool IsProcessed { get; }
     }
 
     public interface ITypedRequestInfo<out RequestType> : IRequestInfo
--- src/LogicBlock/LogicBlock/Info/Implementation/RequestInfo.cs
+++ src/LogicBlock/LogicBlock/Info/Implementation/RequestInfo.cs
@@ -4,39 +4,12 @@ namespace LogicBlock.Info
 {
     public abstract class RequestInfo : IRequestInfo
     {
-        public Action<IResponseInfo> OnFail;
-        public Action<IResponseInfo> OnSuccess;
         
         public IOperationRequest OperationRequest { get; set; }
-        public byte Status { get; protected set; }
-        public bool IsNew => Status == CallStatus.New;
-        public bool IsSucceeded => Status == CallStatus.Succeeded;
-        public bool IsFailed => Status == CallStatus.Failed;
-        public bool IsProcessed => Status >= CallStatus.Succeeded;
 
         public RequestInfo()
         {
 
-        }
-
-        public void Success(IResponseInfo message)
-        {
-            if (IsProcessed)
-                throw new Exception("Already called Success/Fail");
-            
-            OnSuccess?.Invoke(message);
-
-            Status = CallStatus.Succeeded;
-        }
-
-        public void Fail(IResponseInfo message)
-        {
-            if (IsProcessed)
-                throw new Exception("Already called Success/Fail");
-            
-            Status = CallStatus.Failed;
-
-            OnFail?.Invoke(message);
         }
     }
 
