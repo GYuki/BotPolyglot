b51fce24033c80d48e49d921d9bba931f007ec10
Sergey <seri05@yandex.ru>
2020-04-08 23:57:33 +0300

    moved session to logicblock project

--- src/LogicBlock/TranslationsEF/Infrastructure/Repositories/ISessionRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace LogicBlock.Session.Repositories
-{
-    public interface ISessionRepository
-    {
-        Task<ChatSession> GetSessionAsync(int chatId);
-    }
-}
