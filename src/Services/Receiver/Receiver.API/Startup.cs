296b7fde4241d02d70b0c83e81176c3c43331758
Sergey <seri05@yandex.ru>
2020-04-12 20:28:27 +0300

    startup configuration

--- src/Services/Receiver/Receiver.API/Startup.cs
+++ src/Services/Receiver/Receiver.API/Startup.cs
@@ -1,10 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Threading.Tasks;
+using LogicBlock.Logic;
+using LogicBlock.Translations.Infrastructure;
+using LogicBlock.Translations.Infrastructure.Repositories;
+using LogicBlock.Translations.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +21,40 @@ namespace Receiver
 {
     public class Startup
     {
+        public Startup(IHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options => {
+                var port = GetDefinedPorts(Configuration);
+                options.Listen(IPAddress.Any, port, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                });
+            });
+
+            services.AddDbContext<TranslationContext>(opt =>
+                opt.UseMySql(Configuration.GetConnectionString("connection"), mySqlOptionsAction: sqlOpt =>
+                {
+                    sqlOpt.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                    sqlOpt.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                }));
+            
+            services.AddTransient(typeof(ILanguageRepository<AbstractLanguage>), typeof(LanguageRepository<RuLanguage>));
+            services.AddTransient<IArcadeLogic, ArcadeLogic>();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +65,20 @@ namespace Receiver
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+        }
+
+        private int GetDefinedPorts(IConfiguration config)
+        {
+            var port = config.GetValue("PORT", 80);
+            return port;
         }
     }
 }
