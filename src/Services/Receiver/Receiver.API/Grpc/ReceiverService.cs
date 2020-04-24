859006ae99ae995ad2461fd3573efc177cf7518d
Sergey <seri05@yandex.ru>
2020-04-25 00:30:42 +0300

    implemented handlereceiver request

--- src/Services/Receiver/Receiver.API/Grpc/ReceiverService.cs
+++ src/Services/Receiver/Receiver.API/Grpc/ReceiverService.cs
@@ -5,6 +5,8 @@ using LogicBlock.Info;
 using LogicBlock.Logic;
 using LogicBlock.Session;
 using Microsoft.Extensions.Logging;
+using Receiver.API.Infrastructure.LogicController;
+using Receiver.API.Models;
 
 namespace GrpcReceiver
 {
@@ -12,12 +14,57 @@ namespace GrpcReceiver
     {
         private readonly IArcadeLogic _arcade;
         private readonly ITutorialLogic _tutorial;
+
+        private readonly ILogicController _logic;
         private readonly ILogger<ReceiverService> _logger;
 
         public ReceiverService(IArcadeLogic arcade, ITutorialLogic tutorial)
         {
             _arcade = arcade;
             _tutorial = tutorial;
+        }
+        
+        public ReceiverService(ILogicController logic, ILogger<ReceiverService> logger)
+        {
+            _logic = logic;
+            _logger = logger;
+        }
+
+        public override async Task<ChatResponse> HandleReceiverRequest(ActionRequest action, ServerCallContext context)
+        {
+            _logger.LogInformation("receiverService.HandleReceiverRequest()");
+
+            ChatSession chatSession = MapToChatSession(action.Request);
+
+            var currentState = _logic.GetLogic(chatSession.State);
+
+            if (currentState == null)
+            {
+                context.Status = new Status(StatusCode.Internal, $"State {chatSession.State.ToString()} is not found in receiver logic controller");
+                return null;
+            }
+
+            var response = new ChatResponse()
+            {
+                Session = MapToChatRequest(chatSession)
+            };
+
+            switch(action.Message)
+            {
+                case "back":
+                    response.Message = currentState.Back(chatSession);
+                    break;
+                case "menu":
+                    response.Message = currentState.Menu(chatSession);
+                    break;
+                default:
+                    var actionResult = await currentState.Act(action.Message, chatSession);
+                    response = ResponseModelToChatResponse(actionResult);
+                    break;
+            }
+
+            context.Status = new Status(StatusCode.OK, $"Success action");
+            return response;
         }
 
         public override async Task<ChatResponse> HandleArcadeStart(ChatRequest request, ServerCallContext context)
@@ -113,6 +160,16 @@ namespace GrpcReceiver
             return result;
         }
 
+        
+        private ChatResponse ResponseModelToChatResponse(ResponseModel model)
+        {
+            return new ChatResponse
+            {
+                Message = model.Message,
+                Session = MapToChatRequest(model.Session),
+                Success = model.Success
+            };
+        }
         private ChatResponse MapToChatResponse(IResponseInfo responseInfo, ChatSession session)
         {
             return new ChatResponse
