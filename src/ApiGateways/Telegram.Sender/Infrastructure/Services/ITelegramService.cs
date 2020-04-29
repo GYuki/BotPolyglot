af82a3cc47d52e9a7259e7a0bbed16436deda388
Sergey <seri05@yandex.ru>
2020-04-29 21:34:00 +0300

    implemented telegram service

---
+++ src/ApiGateways/Telegram.Sender/Infrastructure/Services/ITelegramService.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace ApiGateways.Telegram.Sender.Infrastructure.Services
+{
+    public interface ITelegramService
+    {
+        Task<Message> SendTextMessageAsync(
+            string botToken,
+            ChatId chatId,
+            string text,
+            ParseMode parseMode = default,
+            bool disableWebPagePreview = default,
+            bool disableNotification = default,
+            int replyToMessageId = default,
+            IReplyMarkup replyMarkup = default,
+            CancellationToken cancellationToken = default
+        );
+    }
+}
