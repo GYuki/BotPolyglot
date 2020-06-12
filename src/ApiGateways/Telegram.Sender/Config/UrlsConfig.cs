83187ecbc5b80a87b8f46b4e1a791dc9d722bbd7
Sergey <seri05@yandex.ru>
2020-06-12 04:16:47 +0300

    urls to en service in configs

--- src/ApiGateways/Telegram.Sender/Config/UrlsConfig.cs
+++ src/ApiGateways/Telegram.Sender/Config/UrlsConfig.cs
@@ -6,5 +6,14 @@ namespace ApiGateways.Telegram.Sender.Config
     {
         public string Session { get; set; }
         public string Receiver { get; set; }
+        public string ReceiverEn { get; set; }
+
+        public string Receivers(string input) =>
+         input switch
+         {
+             "ru" => Receiver,
+             "en" => ReceiverEn,
+             _ => Receiver
+         };
     }
 }
